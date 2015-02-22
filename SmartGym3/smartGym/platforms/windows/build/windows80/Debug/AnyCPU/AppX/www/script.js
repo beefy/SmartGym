713f8361a227dbfe1051a468e440a14ae0a58cdd
unknown <nate.schultz@outlook.com>
2015-02-22 02:42:45 -0500

    fixed image sizes

---
+++ SmartGym3/smartGym/platforms/windows/build/windows80/Debug/AnyCPU/AppX/www/script.js
@@ -0,0 +1,19 @@
+﻿function vidClicked() {
+    window.location.replace("vid.html");
+}
+
+function genfitClicked() {
+    window.location.replace("genfit.html");
+}
+
+function yourwrkoutClicked() {
+    window.location.replace("yourworkout.html");
+}
+
+function tipsClicked() {
+    window.location.replace("tips.html");
+}
+function returnHome()
+{
+    window.location.replace("index.html");
+}

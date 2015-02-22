ad375f0486e713ad2a2940783f97ed05a17f26fb
Derek Alonso <da524@drexel.edu>
2015-02-21 19:38:35 -0500

    Added buttton support

---
+++ SmartGym3/smartGym/platforms/windows/build/windows/Debug/AnyCPU/AppX/www/script.js
@@ -0,0 +1,33 @@
+﻿function vidPlay() {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var player;
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            height: '390',
+            width: '640',
+            videoId: 'Dy28eq2PjcM',
+        });
+    }
+}
+function vidClicked() {
+    window.location.replace("vid.html");
+}
+
+function genfitClicked() {
+    window.location.replace("genfit.html");
+}
+
+function yourwrkoutClicked() {
+    window.location.replace("yourworkout.html");
+}
+
+function tipsClicked() {
+    window.location.replace("tips.html");
+}
+function returnHome()
+{
+    window.location.replace("index.html");
+}
